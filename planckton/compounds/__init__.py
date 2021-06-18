27465bee5a7fde3e9bef8db1d7dfacac89812a03
Bryan Wright <bryaniswright@gmail.com>
2021-06-18 16:12:38 -0600

    saving my files

--- planckton/compounds/__init__.py
+++ planckton/compounds/__init__.py
@@ -4,7 +4,7 @@ import os
 
 COMPOUND_DIR = os.path.abspath(os.path.dirname(__file__))
 COMPOUND = {}
-for compound_file in glob.glob(os.path.join(COMPOUND_DIR, "*_typed.mol2")):
+for compound_file in glob.glob(os.path.join(COMPOUND_DIR, "*.mol2")):
     # The the last part of the path, drop the _typed.mol2
     compound_name = compound_file.split("/")[-1].split("_typed.mol2")[0]
     COMPOUND[compound_name] = compound_file
